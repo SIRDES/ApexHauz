0dc226137e063bbb1560b0d94e41d0d9dd31f93c
sirdes <desmondtetteh1.dt@gmail.com>
2022-05-13 10:11:12 +0000

    Feat: user can view a property

--- src/routes/property.route.js
+++ src/routes/property.route.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const property = require("../controllers/property.controller");
-const auth = require("../middleware/auth")
+const auth = require("../middleware/auth");
+
 module.exports = (app) => {
   router.post("/", auth, property.create);
   router.get("/", property.getAll);
+  router.get("/:id", property.getOne);
 
   app.use("/api/v1/property", router);
 };
